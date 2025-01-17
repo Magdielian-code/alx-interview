1a50f924df094154fd1da219fad0d3bdad5b4e98
magdielian-code <oziomaagaecheta81@gmail.com>
2025-01-17 17:40:09 +0100

    fixed algorithmns and data structures

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,91 +1,61 @@
-
 #!/usr/bin/python3
-"""
-N queens puzzle
-"""
+"""N Queens placement on NxN chessboard"""
 
 
 import sys
 
 
-def is_safe(board, row, col):
-    # Check if placing a queen at position (row, col) is safe
-    # Check the row
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check lower diagonal on left side
-    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
+def generate_solutions(row, column):
+    solution = [[]]
+    for queen in range(row):
+        solution = place_queen(queen, column, solution)
+    return solution
 
 
-def solve_nqueens(N, board, col, solutions):
-    # Base case: If all queens are placed, add current solution
-    # to solutions list
-    if col >= N:
-        solution = []
-        for i in range(N):
-            for j in range(N):
-                if board[i][j] == 1:
-                    solution.append([i, j])
-        solutions.append(solution)
-        return
-
-    # Consider this column and try placing this queen in all rows one by one
-    for i in range(N):
-        if is_safe(board, i, col):
-            board[i][col] = 1
-            solve_nqueens(N, board, col + 1, solutions)
-            board[i][col] = 0
+def place_queen(queen, column, prev_solution):
+    safe_position = []
+    for array in prev_solution:
+        for x in range(column):
+            if is_safe(queen, x, array):
+                safe_position.append(array + [x])
+    return safe_position
 
 
-def print_solutions(solutions):
-    """
-    Print the solutions in the specified format.
-
-    Parameters:
-        solutions (list): A list of solutions,
-        where each solution is a list of queen positions.
-
-    Returns:
-        None
-    """
-    return [print(solution) for solution in solutions]
+def is_safe(q, x, array):
+    if x in array:
+        return (False)
+    else:
+        return all(abs(array[column] - x) != q - column
+                   for column in range(q))
 
 
-def main():
-    # Input validation
+def init():
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
+    if sys.argv[1].isdigit():
+        n = int(sys.argv[1])
+    else:
         print("N must be a number")
         sys.exit(1)
-
-    if N < 4:
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-
-    # Initialize an empty chessboard
-    board = [[0 for _ in range(N)] for _ in range(N)]
-
-    # Find and print all solutions
-    solutions = []
-    solve_nqueens(N, board, 0, solutions)
-    print_solutions(solutions)
+    return (n)
 
 
-if __name__ == "__main__":
-    main()
+def n_queens():
+
+    n = init()
+    # generate all solutions
+    solutions = generate_solutions(n, n)
+    # print solutions
+    for array in solutions:
+        clean = []
+        for q, x in enumerate(array):
+            clean.append([q, x])
+        print(clean)
+
+
+if __name__ == '__main__':
+    n_queens()
